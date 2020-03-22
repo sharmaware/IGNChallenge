f75486890b79f94e6bd1dce234229ba353fec039
Saarthak Sharma <sharma.saarthak@gmail.com>
2020-03-22 16:50:11 -0700

    returning path

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include <fstream>
 #include <vector>
 #include <list>
-//#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-struct quest{
+class quest{
+public:
     string name;
     int start;
     int duration;
@@ -46,7 +47,7 @@ void loadQuests(string path, list<quest> &quests){
     
 }
 
-int maxReward(list<quest> quests, int reward);
+int maxReward(list<quest> quests, int reward, vector<quest> &order);
 
 int main(int argc, const char * argv[]) {
     //using list for fast erasing from front
@@ -57,38 +58,59 @@ int main(int argc, const char * argv[]) {
     
     
     for(auto it = quests.begin(); it != quests.end(); it++){
-        cout << it->name << " " << it->start << " " << it->duration << " " << it->reward << " " <<it->reward/it->duration << endl;
+        cout << it->name << "         " << it->start << "      " << it->duration << "       " << it->reward << "       " <<it->reward/it->duration << endl;
     }
 
-    int max = maxReward(quests, 0);
+    vector<quest> order;
+    int max = maxReward(quests, 0, order);
     cout << max << endl;
+    
+    cout << order.size() << endl;
+    
+    for(int i = 0; i < order.size(); i++){
+        cout << order[i].name << " " << order[i].start << " " << order[i].duration <<  " " << order[i].reward <<  endl;
+    }
     
 }
 
 //create map to return values?
-int maxReward(list<quest> quests, int reward/*, vector<quest> &bestOrder*/){
+//reconstruct solution by passing copy by reference in end of function
+
+int maxReward(list<quest> quests, int reward, vector<quest> &order){
     if(quests.size() == 0){
         return reward;
     }
     
     quest current = quests.front();
-    cout << quests.size() << " " << current.name<< endl;
+    
     //finding max without current
     quests.pop_front();
-    int maxNoCurrent = maxReward(quests, reward);
+    vector<quest> noCurrentOrder;
+    int maxNoCurrent = maxReward(quests, reward, noCurrentOrder);
     
-    //removing quests that overlap
+    //removing quests that overlap binary search here??
     while(quests.size() > 0 && quests.front().start < current.start + current.duration){
         quests.pop_front();
     }
     //finding max with current
-    int maxCurrent = maxReward(quests, reward+current.reward);
+    vector<quest> currentOrder;
+    int maxCurrent = maxReward(quests, reward+current.reward, currentOrder);
     
     //returning max
     if(maxCurrent > maxNoCurrent){
+        //add members of currentOrder to order
+        for(int i = 0; i < currentOrder.size(); i++){
+            order.push_back(currentOrder[i]);
+        }
+        order.push_back(current);
+        
         return maxCurrent;
     }
-    return maxNoCurrent;
     
+    for(int i = 0; i < noCurrentOrder.size(); i++){
+        order.push_back(noCurrentOrder[i]);
+    }
+    
+    return maxNoCurrent;
     
 }
